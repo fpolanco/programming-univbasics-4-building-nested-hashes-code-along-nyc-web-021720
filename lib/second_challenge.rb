d2986650c83e2a405e8ec59dd15ddcd34ff355f4
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-22 02:02:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,11 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      { patriarch: {}, matriarch: {}, hero:{}, hero_friends: []
-      },
-    :capulet => {,
-    patriarch: {}, matriarch: {}, heroine:{}, heroine_friends: [] 
-      }}
-
-
-end
+# def second_challenge
+#   epic_tragedy = {
+#     :montague => {
+#       { patriarch: {}, matriarch: {}, hero:{}, hero_friends: []
+#       },
+#     :capulet => {,
+#     patriarch: {}, matriarch: {}, heroine:{}, heroine_friends: []
+#       }}
+#
+#
+# end
