6477d1603ccc65e2716ed9647f00cbc1898d70b2
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-22 02:29:12 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,5 +25,7 @@ def bonus
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy= [:montague][:hero][:status]="dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tradegy[:capulet][:heroine][:status] = "dead"
+  puts epic_tradegy
 end
